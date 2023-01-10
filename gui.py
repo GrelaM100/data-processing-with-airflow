054266f4d625ded97f7f28c9a8f156900ef9eaf8
GRELAM <grelam100@gmail.com>
2023-01-10 03:01:28 +0100

    Added Streamlit GUI

---
+++ gui.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import datetime
+import pandas as pd
+import pydeck as pdk
+
+date_format = 'DD-MM-YYYY HH:mm'
+forecasts = pd.read_parquet('forecasts.parquet')
+forecasts['datetime'] = pd.to_datetime(forecasts['datetime'])
+start_date = forecasts['datetime'].min().to_pydatetime()
+end_date = forecasts['datetime'].max().to_pydatetime()
+
+slider = st.slider('Select date',
+                   min_value=start_date,
+                   value=start_date,
+                   max_value=end_date,
+                   step=datetime.timedelta(hours=1),
+                   format=date_format)
+
+
+plot_data = forecasts[forecasts['datetime'] == slider]
+plot_data['max_temp'] = plot_data['temperature'].max()
+plot_data['min_temp'] = plot_data['temperature'].min()
+tooltip = {
+    "html":
+        "<b>City:</b> {city} <br/>"
+        "<b>Temperature:</b> {temperature}\N{DEGREE SIGN}C<br/>",
+    "style": {
+        "backgroundColor": "steelblue",
+        "color": "black",
+    }
+}
+st.pydeck_chart(pdk.Deck(
+    map_provider='mapbox',
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=51.91,
+        longitude=19.14,
+        zoom=5
+    ),
+    layers=[
+        pdk.Layer(
+            type='ScatterplotLayer',
+            data=plot_data,
+            pickable=True,
+            opacity=0.8,
+            radius_scale=6,
+            radius_min_pixels=5,
+            radius_max_pixels=100,
+            line_width_min_pixels=1,
+            onClick=True,
+            filled=True,
+            get_position=['longitude', 'latitude'],
+            get_color=['((temperature - min_temp) * 255) / (max_temp - min_temp)', 0, 0],
+            get_line_color=[0, 0, 0])
+    ],
+    tooltip=tooltip)
+)
+
+
+st.dataframe(plot_data[['city', 'temperature']].reset_index(drop=True), width=400)
